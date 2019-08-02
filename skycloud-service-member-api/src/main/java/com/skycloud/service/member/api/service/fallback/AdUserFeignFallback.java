f764f3220b0cbd10f032b17894de45f762b7b994
shen11961677@163.com <shenliwei521>
2019-08-02 10:02:05 +0800

    init

--- skycloud-service-member-api/src/main/java/com/skycloud/service/member/api/service/fallback/AdUserFeignFallback.java
+++ skycloud-service-member-api/src/main/java/com/skycloud/service/member/api/service/fallback/AdUserFeignFallback.java
@@ -1,8 +1,30 @@
+/*
+ * The MIT License (MIT)
+ * Copyright © 2019 <sky>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
 package com.skycloud.service.member.api.service.fallback;
 
-import com.sky.framework.common.LogUtil;
+import com.sky.framework.common.LogUtils;
 import com.sky.framework.model.dto.MessageRes;
-import com.sky.framework.model.enums.FailureCodeEnum;
+import com.skycloud.base.common.hystrix.HystrixFallBackResult;
 import com.skycloud.service.member.api.model.dto.CustomLoginDto;
 import com.skycloud.service.member.api.service.AdUserFeignApi;
 import lombok.Setter;
@@ -11,7 +33,6 @@ import lombok.extern.slf4j.Slf4j;
 /**
  * @author
  */
-//@Component
 @Slf4j
 public class AdUserFeignFallback implements AdUserFeignApi {
 
@@ -20,8 +41,8 @@ public class AdUserFeignFallback implements AdUserFeignApi {
 
     @Override
     public MessageRes<CustomLoginDto> login(CustomLoginDto customLoginDto) {
-        LogUtil.error(log, "feign fallback :{} ", cause);
-        return MessageRes.fail(FailureCodeEnum.GL990002.getCode(), FailureCodeEnum.GL990002.getMsg());
+        LogUtils.error(log, "feign fallback :{} ", cause);
+        return HystrixFallBackResult.fail(cause);
     }
 
 }
